af458abc90e7d2d9398eee4bdd5d0ca8df2307d3
himanshu198666 <39584372+himanshu198666@users.noreply.github.com>
2021-06-28 23:02:00 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,14 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 const Story = require("../models/Story");
 
-// @desc    Login/Landing page
-// @route   GET /
+
 router.get("/", ensureGuest, (req, res) => {
   res.render("login", {
     layout: "login",
   });
 });
 
-// @desc    Dashboard
-// @route   GET /dashboard
+
 router.get("/dashboard", ensureAuth, async (req, res) => {
   try {
     const stories = await Story.find({ user: req.user.id }).lean();
