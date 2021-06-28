1646d761be9761004180277ea1ad2730e30d50ac
himanshu198666 <39584372+himanshu198666@users.noreply.github.com>
2021-06-28 23:01:24 +0530

    Update stories.js

--- routes/stories.js
+++ routes/stories.js
@@ -4,14 +4,10 @@ const { ensureAuth } = require("../middleware/auth");
 
 const Story = require("../models/Story");
 
-// @desc    Show add page
-// @route   GET /stories/add
 router.get("/add", ensureAuth, (req, res) => {
   res.render("stories/add");
 });
 
-// @desc    Process add form
-// @route   POST /stories
 router.post("/", ensureAuth, async (req, res) => {
   try {
     req.body.user = req.user.id;
@@ -23,8 +19,6 @@ router.post("/", ensureAuth, async (req, res) => {
   }
 });
 
-// @desc    Show all stories
-// @route   GET /stories
 router.get("/", ensureAuth, async (req, res) => {
   try {
     const stories = await Story.find({ status: "public" })
@@ -41,8 +35,6 @@ router.get("/", ensureAuth, async (req, res) => {
   }
 });
 
-// @desc    Show single story
-// @route   GET /stories/:id
 router.get("/:id", ensureAuth, async (req, res) => {
   try {
     let story = await Story.findById(req.params.id).populate("user").lean();
@@ -64,8 +56,7 @@ router.get("/:id", ensureAuth, async (req, res) => {
   }
 });
 
-// @desc    Show edit page
-// @route   GET /stories/edit/:id
+
 router.get("/edit/:id", ensureAuth, async (req, res) => {
   try {
     const story = await Story.findOne({
@@ -89,8 +80,7 @@ router.get("/edit/:id", ensureAuth, async (req, res) => {
   }
 });
 
-// @desc    Update story
-// @route   PUT /stories/:id
+
 router.put("/:id", ensureAuth, async (req, res) => {
   try {
     let story = await Story.findById(req.params.id).lean();
@@ -115,8 +105,6 @@ router.put("/:id", ensureAuth, async (req, res) => {
   }
 });
 
-// @desc    Delete story
-// @route   DELETE /stories/:id
 router.delete("/:id", ensureAuth, async (req, res) => {
   try {
     let story = await Story.findById(req.params.id).lean();
@@ -137,8 +125,7 @@ router.delete("/:id", ensureAuth, async (req, res) => {
   }
 });
 
-// @desc    User stories
-// @route   GET /stories/user/:userId
+
 router.get("/user/:userId", ensureAuth, async (req, res) => {
   try {
     const stories = await Story.find({
