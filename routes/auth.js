c25859a14898a008be64934f510026101041533e
himanshu198666 <39584372+himanshu198666@users.noreply.github.com>
2021-06-28 23:02:17 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,10 @@ const express = require("express");
 const passport = require("passport");
 const router = express.Router();
 
-// @desc    Auth with Google
-// @route   GET /auth/google
+
 router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
 
-// @desc    Google auth callback
-// @route   GET /auth/google/callback
+
 router.get(
   "/google/callback",
   passport.authenticate("google", { failureRedirect: "/" }),
@@ -16,8 +14,6 @@ router.get(
   }
 );
 
-// @desc    Logout user
-// @route   /auth/logout
 router.get("/logout", (req, res) => {
   req.logout();
   res.redirect("/");
